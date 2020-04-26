e947531a936286a06aa684a65273e2eaca226d05
sumits557 <sumits557@gmail.com>
2020-04-26 12:25:47 -0400

    Few examples of linked list algorithms.

---
+++ src/main/java/linkedlist/DoubleEndedList.java
@@ -0,0 +1,16 @@
+package src.main.java.linkedlist;
+
+public class DoubleEndedList<T> extends LinkedList<T> {
+
+    private Node<T> tail;
+
+    public Node<T> getTail() {
+        return this.tail;
+    }
+
+    public void addAtEnd(T data) {
+        Node<T> newNode = new Node<T>(data);
+        this.tail.setNextNode(newNode);
+        this.tail = newNode;
+    }
+}
